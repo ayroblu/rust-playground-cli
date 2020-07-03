e3c649c36b9e054ca74f85e0bb632f1c4e1d7a01
Ben Lu <ayroblu@gmail.com>
2020-07-04 00:01:58 +0100

    Using enums

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,81 @@
-use clap::{App, Arg};
+// https://github.com/clap-rs/clap/blob/master/examples/20_subcommands.rs
+use clap::{App, AppSettings, Arg, ArgMatches};
+mod structs;
 
 fn main() {
-    let matches = App::new("Rust Playground CLI")
+    let matches = define_clap_definition();
+    handle_matches(matches)
+}
+
+fn define_clap_definition() -> ArgMatches {
+    return App::new("Rust Playground CLI")
+        .version("1.0.1")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(
-            App::new("test").arg(
-                Arg::new("config")
-                    .short('c')
-                    .long("config")
-                    .value_name("FILE")
-                    .about("Sets a custom config file")
-                    .takes_value(true),
-            ),
+            App::new(MainCommands::CloneCmd.as_str())
+                .about("clones repos")
+                .arg(Arg::new("repo").about("The repo to clone").required(true)),
+        )
+        .subcommand(
+            App::new(MainCommands::Add.as_str())
+                .setting(AppSettings::ArgRequiredElseHelp) // They can even have different settings
+                .arg(
+                    Arg::new("stuff")
+                        .long("stuff")
+                        .about("Stuff to add")
+                        .takes_value(true)
+                        .multiple(true),
+                ),
         )
         .get_matches();
+}
 
-    println!("Value for config: {:?}", matches);
+fn handle_matches(matches: ArgMatches) {
+    // The most common way to handle subcommands is via a combined approach using
+    // `ArgMatches::subcommand` which returns a tuple of both the name and matches
+    let (cmd, matches_opt) = matches.subcommand();
+    let subcmd_matches = (MainCommands::from_str(cmd).unwrap(), matches_opt);
 
-    if let Some(ref matches) = matches.subcommand_matches("test") {
-        if matches.is_present("config") {
-            println!("Test with config");
-        } else {
-            println!("Normal test");
+    match subcmd_matches {
+        (MainCommands::CloneCmd, Some(clone_matches)) => {
+            structs::methods::show_struct();
+            // Now we have a reference to clone's matches
+            println!("Cloning {}", clone_matches.value_of("repo").unwrap());
+        }
+        (MainCommands::Add, Some(add_matches)) => {
+            // Now we have a reference to add's matches
+            println!(
+                "Adding {}",
+                add_matches
+                    .values_of("stuff")
+                    .unwrap()
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
+        // If all subcommands are defined above, anything else is unreachabe!()
+        (_, None) => unreachable!(),
+    }
+}
+
+enum MainCommands {
+    CloneCmd,
+    Add,
+}
+
+impl MainCommands {
+    pub fn from_str(s: &str) -> Option<MainCommands> {
+        match s {
+            "clone" => Some(MainCommands::CloneCmd),
+            "add" => Some(MainCommands::Add),
+            _ => None,
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            MainCommands::CloneCmd => "clone",
+            MainCommands::Add => "add",
         }
     }
 }
